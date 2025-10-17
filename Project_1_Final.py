0fe56fe2098b6b5f066ceedc467baafbb7e4ded6
Charlotte Norment <charlottenorment@Charlottes-MacBook-Air-543.local>
2025-10-17 01:01:46 -0400

    Third Commit: functions for calculation 2

--- Project_1_Final.py
+++ Project_1_Final.py
@@ -51,16 +51,6 @@ def load_penguins(csv_file_path):
 return penguin_data                           
 
 
-def main():
-    # define file path 
-    data_file = 'penguins.csv'
-
-    # define chosen island for calculation 2
-    chosen_island = 'Dream'
-
-    # call load_penguins to load the data
-    penguin_data = load_penguins(data_file)
-
 
 # Step 2: Calculation 1 
 
@@ -120,18 +110,94 @@ def calculate_flipper_percentages(data):
     return flipper_results 
 
 
+
 # Step 3: Calculation 2
     def calculate_island_averages(data, chosen_island):
         total_length = 0.0
         total_depth = 0.0
         count = 0 
 
-        # loop through 
+        # loop through penguin records
+        for record in data:
+        
+            # filter to see if the record is for the chosen island
+            if record.get('island') == chosen_island:
+            
+                # get the data 
+                bill_length = record.get('bill_length_mm')
+                bill_depth = record.get('bill_depth_mm')
+
+                # check for missing data 
+                if bill_length is not None and bill_depth is not None:
+                    total_length += bill_length
+                    total_depth += bill_depth 
+                    count += 1 
+
+        # calculate averages 
+        if count > 0:
+            avg_length = total_length / count
+            avg_depth = total_depth / count 
+
+            island_results = {
+                'Island Name': chosen_island,
+                'Average Bill Length (mm)': round(avg_length, 2),
+                'Average Bill Depth (mm)': round(avg_depth, 2),
+                'Sample Size (Count)': count 
+            }
+        
+        # account for cases where island does not have valid data
+        else:
+            island_results = {
+                'Island Name': chosen_island,
+                'Error': 'No valid data for island',
+                'Sample Size (Count)': 0 
+            }
+
+        return island_results 
+
+
 
 # Step 4: Report Results
-    #def write_report():
+    def write_report(flipper_results, island_results, output_file_path):
+        
+        with open(output_file_path, 'w') as f:
+            f.write("\n Penguin Data Analysis Report \n")
+
+        # write report for calculation 1
+        f.write("Calculation 1: Flipper Lengths > 200mm  \n")
+
+        for category, results in flipper_results.items():
+            f.write(f"Analysis by {category}:\n")
+
+            if results:
+                for group, percentage in results.items():
+                # make sure string is clean
+                cleaned_percent = f"{percentage * 100:.1f}%"
+                f.write(f" {group.1just(10)}: {cleaned_percent}\n")
+
+            else:
+                f.write("No data found for this category\n")
+            f.write("\n")
+
+        # write report for calculation 2
+        f.write("Calculation 2: Averages for {island_results['Island Name']} Island  \n")
+        if 'Error' in island_results:
+            f.write(f"Error: {island_results['Error']}")
+
+
+
 
 #call to main
+def main():
+    # define file path 
+    data_file = 'penguins.csv'
+
+    # define chosen island for calculation 2
+    chosen_island = 'Dream'
+
+    # call load_penguins to load the data
+    penguin_data = load_penguins(data_file)
+
 if __name__ == "__main__":
     csv_data = load_penguins("penguins.csv")
     for row in csv_data:
